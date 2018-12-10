5423e0bdb0264ed82e2f1d4f28516b5187b1dcb9
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-12-10 10:41:27 -0500

    adds Routes and Links from the react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import {
+	BrowserRouter as Router, // watches for changes to URL
+	Route,	// Renders components based on the specific URL
+	Link	// Changes the URL so the Router sees it
+} from 'react-router-dom';
+
 import Home from './Home';
 import About from './About';
 import Cats from './Cats';
@@ -8,15 +14,22 @@ import Cats from './Cats';
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Home/>
-        <About/>
-        <Cats/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<ul>
+						<li><Link to="/">Home</Link></li>
+						<li><Link to="/about">About</Link></li>
+						<li><Link to="cats">Cats</Link></li>
+					</ul>
+					<Route path="/" exact component={Home}/>
+					<Route path="/about" component={About}/>
+					<Route path="/cats" component={Cats}/>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
