ba1cfe1d4927d640fa171c2ea1823862e9c43dc8
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-12-10 11:27:48 -0500

    makes individual cats a link in cat component

--- src/Cats.js
+++ src/Cats.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Cats(props) {
     return (
         <div>
             <h1>Cats</h1>
+            <ul>
+                <li>
+                    <Link to="/cats/oakley">Oakley</Link>
+                </li>
+                <li>
+                    <Link to="/cats/milla">Millas</Link>
+                </li>
+            </ul>
         </div>
     );
 };
